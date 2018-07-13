d41d27dfa6d05564a3f6a01428170ff8b98fff94
sideroff <ivan.sideroff@gmail.com>
2018-07-13 18:07:31 +0300

    refactoring

--- app/components/MainApp.js
+++ app/components/MainApp.js
@@ -1,29 +1,7 @@
-import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
-
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
-
 
 import Home from './Home'
 import Account from './Account'
-
-class MainApp extends Component {
-
-  getCorrectComponent() {
-
-  }
-
-  render() {
-    return (
-      <View >
-        <Text>Main App</Text>
-        <Button title='logout' onPress={() => { this.props.dispatch(logout()) }} />
-      </View>
-    )
-  }
-}
-
-// export default connect(mapStateToProps, dispatch => ({ dispatch }))(MainApp)
 
 export default Navigator = createMaterialBottomTabNavigator(
   {
