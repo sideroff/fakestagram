bb00cea5df3facc85b0760457d2de01c2e45aba6
Ivan Siderov <ivan.siderov@corp.accedia.com>
2018-08-15 11:29:40 +0300

    firebase fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native'
+import React from 'react'
+import { AppRegistry, View, Text } from 'react-native'
 import App from './app/index'
 import { name as appName } from './app.json'
 
